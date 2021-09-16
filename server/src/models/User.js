a4a3f749dca4e8eb25d06f293839381f466d5589
Adrian Grillo <grillo.adrian13@gmail.com>
2021-09-16 18:25:00 -0400

    added user password salting and hashing

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -10,6 +11,30 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+})
+
+userSchema.pre('save', function(next) {
+    const user = this
+
+    // If the user hasn't modified their password in any way, don't salt anything.
+    if(!user.isModified('password')) {
+        return next()
+    }
+
+    // Generate salt and assign it to the users password before storing it in mongodb.
+    bcrypt.genSalt(10, (err, salt) => {
+        if(err) { 
+            return next(err)
+        } 
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if(err) {
+                return next(err)
+            }
+            user.password = hash
+            next()
+        })
+    })
 })
 
 mongoose.model('User', userSchema)
